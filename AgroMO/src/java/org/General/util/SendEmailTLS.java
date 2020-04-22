850e703782e8e473c5a08b8ca3503b929c20d735
Minion86 <nelson_martinez86@hotmail.com>
2020-04-22 10:33:34 -0500

    nuevo envio mails

--- AgroMO/src/java/org/General/util/SendEmailTLS.java
+++ AgroMO/src/java/org/General/util/SendEmailTLS.java
@@ -5,50 +5,95 @@
  */
 package org.General.util;
 
+import com.sun.mail.smtp.SMTPTransport;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
 
 public class SendEmailTLS {
 
+    private static final String SMTP_SERVER = "smtp.gmail.com";
+    private static final String USERNAME = "nelsonmartinez86@gmail.com";
+    private static final String PASSWORD = "fzkyrlqiszszwnvj";
+
     public void SendNotificacion(String to, String texto) {
-
-        //final String username = "Renzo199725@gmail.com";
-        final String username = "nelsonmartinez86@gmail.com";
-        //final String password = "089897717r";
-        final String password = "fzkyrlqiszszwnvj";
-
-        Properties prop = new Properties();
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "587");
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true"); //TLS
-
-        Session session = Session.getInstance(prop,
-                new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
 
         try {
 
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("no-reply@gmail.com"));
-            message.setRecipients(
-                    Message.RecipientType.TO,
-                    InternetAddress.parse(to)
-            );
-            message.setSubject("Reporte Análisis Plantaciones Activas AGROMOP");
+            Properties prop = System.getProperties();
+            prop.put("mail.smtp.host", SMTP_SERVER);
+            prop.put("mail.smtp.port", "587");
+            prop.put("mail.smtp.auth", "true");
+            prop.put("mail.smtp.starttls.enable", "true"); //TLS
+            prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
-            message.setContent(texto, "text/html");
-            Transport.send(message);
+            Session session = Session.getInstance(prop, null);
+            Message msg = new MimeMessage(session);
 
-            System.out.println("Done");
+            msg.setFrom(new InternetAddress("no-reply@gmail.com"));
+
+            msg.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to, false));
+
+            msg.setSubject("Reporte Análisis Plantaciones Activas AGROMOP");
+
+            // HTML email
+            msg.setDataHandler(new DataHandler(new HTMLDataSource(texto)));
+
+            SMTPTransport t = (SMTPTransport) session.getTransport("smtp");
+
+            // connect
+            t.connect(SMTP_SERVER, USERNAME, PASSWORD);
+
+            // send
+            t.sendMessage(msg, msg.getAllRecipients());
+
+            System.out.println("Response: " + t.getLastServerResponse());
+
+            t.close();
 
         } catch (MessagingException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    static class HTMLDataSource implements DataSource {
+
+        private String html;
+
+        public HTMLDataSource(String htmlString) {
+            html = htmlString;
+        }
+
+        @Override
+        public InputStream getInputStream() throws IOException {
+            if (html == null) {
+                throw new IOException("html message is null!");
+            }
+            return new ByteArrayInputStream(html.getBytes());
+        }
+
+        @Override
+        public OutputStream getOutputStream() throws IOException {
+            throw new IOException("This DataHandler cannot write HTML");
+        }
+
+        @Override
+        public String getContentType() {
+            return "text/html";
+        }
+
+        @Override
+        public String getName() {
+            return "HTMLDataSource";
         }
     }
 
